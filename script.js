7f9251b92292de2d1e362ca2fba528f6d6024f5d
Stephanie Rice <swright9616@yahoo.com>
2021-04-02 23:12:51 -0500

    add javascript file

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var numberChars = "0123456789";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  
+  var resultArray = [];
+  var userArray = [];
+
+  varnumberChars = prompt ("What length do you want your password between 8 and 128");
+  var numberChars = confirm ("Do you want numbers in your password?");
+  var upperChars = confirm ("Do you want Uppercases in your password?");
+  var lowerChars = confirm ("Do you want Lowercases in your password?");
+  
+  if (numberChars) {
+    resultArray = resultArray.concat(numberChars);
+  }
+
+  if (upperChars) {
+    resultArray = resultArray.concat(upperChars);
+  }
+
+  if (lowerChars) {
+    resultArray = resultArray.concat(lowerChars);
+  }
+  console.log(resultArray)
+
+  for (var i = 0; i < numberChars; i++) {
+    userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]);
+  }
+  return userArray.join("");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
